4f72d68956aae93e078baff5917866e74892acc9
Andreas Hovaldt Hojrup <andreas.hovaldt@gmail.com>
2026-03-10 16:21:58 +0100

    Changed num GPUs

--- scripts/scannet_quick_modified.sh
+++ scripts/scannet_quick_modified.sh
@@ -13,6 +13,6 @@ python main.py \
 --loss_no_object_weight 0.25 \
 --save_separate_checkpoint_every_epoch -1 \
 --checkpoint_dir outputs/scannet_quick_modified \
---ngpus 2 \
+--ngpus 1 \
 --dataset_num_workers 12 \
 --batchsize_per_gpu 8
